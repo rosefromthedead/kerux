a228cdb37cd7f25d4150f81ee1a1cd62227a862c
Tommy Hudson <thomhuds@protonmail.com>
2019-11-10 20:14:13 +0000

    Add more event types

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,1 +1,85 @@
+use ring::digest::{SHA256, digest};
+use serde::{Deserialize, Serialize};
+use serde_canonical::ser::to_string as to_canonical_json;
+use serde_json::{Map, Value as JsonValue};
+
 pub mod room;
+
+/// Turns a serializable value into a `serde_json::Map<String, Value>`.
+///
+/// Panics if the given value cannot be serialized into JSON or if it is not an object.
+pub fn into_json_map(value: impl Serialize) -> Map<String, JsonValue> {
+    match serde_json::to_value(value).expect("given value cannot be serialized into json") {
+        JsonValue::Object(map) => map,
+        _ => panic!("cannot turn non-object value into json object"),
+    }
+}
+
+/// An unhashed (incomplete) Persistent Data Unit for room version 4.
+/// This can only be used to construct a complete, hashed PDU.
+#[derive(Serialize)]
+pub struct UnhashedEvent {
+    pub room_id: String,
+    pub sender: String,
+    pub origin: String,
+    pub origin_server_ts: i64,
+    #[serde(rename = "type")]
+    pub ty: String,
+    pub state_key: Option<String>,
+    pub content: Map<String, JsonValue>,
+    pub prev_events: Vec<String>,
+    pub depth: i64,
+    pub auth_events: Vec<String>,
+    pub redacts: Option<String>,
+    #[serde(skip)]
+    pub unsigned: Option<Map<String, JsonValue>>,
+}
+
+/// A Persistent Data Unit (room event) for room version 4.
+pub struct RoomEventV4 {
+    pub room_id: String,
+    pub sender: String,
+    pub origin: String,
+    pub origin_server_ts: i64,
+    pub ty: String,
+    pub state_key: Option<String>,
+    pub content: Map<String, JsonValue>,
+    pub prev_events: Vec<String>,
+    pub depth: i64,
+    pub auth_events: Vec<String>,
+    pub redacts: Option<String>,
+    pub unsigned: Option<Map<String, JsonValue>>,
+    pub hashes: EventHash,
+    pub signatures: Map<String, JsonValue>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct EventHash {
+    pub sha256: String,
+}
+
+impl UnhashedEvent {
+    /// Turns self into a hashed RoomEventV4 by hashing its contents.
+    ///
+    /// Does not add any signatures.
+    pub fn finalize(self) -> RoomEventV4 {
+        let json = to_canonical_json(&self).unwrap();
+        let sha256 = base64::encode_config(digest(&SHA256, json.as_bytes()).as_ref(), base64::URL_SAFE_NO_PAD);
+        RoomEventV4 {
+           room_id: self.room_id,
+           sender: self.sender,
+           origin: self.origin,
+           origin_server_ts: self.origin_server_ts,
+           ty: self.ty,
+           state_key: self.state_key,
+           content: self.content,
+           prev_events: self.prev_events,
+           depth: self.depth,
+           auth_events: self.auth_events,
+           redacts: self.redacts,
+           unsigned: self.unsigned,
+           hashes: EventHash { sha256 },
+           signatures: Map::new(),
+        }
+    }
+}
--- src/events/room.rs
+++ src/events/room.rs
@@ -1,6 +1,77 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value as JsonValue;
 use std::collections::HashMap;
 
+/// m.room.create
+#[derive(Deserialize, Serialize)]
+pub struct Create {
+    pub creator: String,
+    pub room_version: Option<String>,
+    pub predecessor: Option<PreviousRoom>,
+    #[serde(flatten)]
+    pub extra: HashMap<String, JsonValue>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct PreviousRoom {
+    pub room_id: String,
+    pub event_id: String,
+}
+
+/// m.room.join_rules
+#[derive(Deserialize, Serialize)]
+pub struct JoinRules {
+    pub join_rule: JoinRule,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum JoinRule {
+    Public,
+    Knock,
+    Invite,
+    Private,
+}
+
+/// m.room.history_visibility
+#[derive(Deserialize, Serialize)]
+pub struct HistoryVisibility {
+    pub history_visibility: HistoryVisibilityType,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum HistoryVisibilityType {
+    Invited,
+    Joined,
+    Shared,
+    WorldReadable,
+}
+
+/// m.room.guest_access
+#[derive(Deserialize, Serialize)]
+pub struct GuestAccess {
+    pub guest_access: GuestAccessType,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum GuestAccessType {
+    CanJoin,
+    Forbidden,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Name {
+    pub name: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Topic {
+    pub topic: String,
+}
+
+/// m.room.power_levels
 #[derive(Deserialize, Serialize)]
 pub struct PowerLevels {
     ban: u32,
@@ -43,4 +114,23 @@ impl Default for Notifications {
             room: 50,
         }
     }
+}
+
+/// m.room.member
+#[derive(Deserialize, Serialize)]
+pub struct Member {
+    pub avatar_url: Option<String>,
+    pub displayname: Option<String>,
+    pub membership: Membership,
+    pub is_direct: bool,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Membership {
+    Invite,
+    Join,
+    Knock,
+    Leave,
+    Ban,
 }
