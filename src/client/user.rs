ddd19a0663f49af299b5ff1ae927fd3c5d8b21bf
Rose Hudson <rose@krx.sh>
2019-12-07 12:02:23 +0000

    Migrate user section of CS API to actix

--- src/client/user.rs
+++ src/client/user.rs
@@ -1,109 +1,119 @@
-use percent_encoding::percent_decode_str;
-use serde_json::json;
+use actix_web::{
+    web::{Data, Json, Path},
+    get, post,
+};
+use serde_json::{json, Value as JsonValue};
 use std::sync::Arc;
-use tide::{response, Context};
 
 use crate::{
     client::{
-        auth::get_access_token,
+        auth::AccessToken,
         error::Error,
-        ClientResult
     },
     ServerState,
 };
 
-pub async fn get_avatar_url(cx: Context<Arc<ServerState>>) -> ClientResult {
-    let user_param = cx.param::<String>("user_id")?;
-    let user_id = percent_decode_str(&user_param).decode_utf8()
-        .map_err(|e| format!("{}", e)).map_err(Error::InvalidParam)?;
+#[get("/profile/{user_id}/avatar_url")]
+pub async fn get_avatar_url(
+    state: Data<Arc<ServerState>>,
+    user_id: Path<String>
+) -> Result<Json<JsonValue>, Error> {
     let user_id = user_id.trim_start_matches('@');
-    let (username, domain) = {
+    let (_username, domain) = {
         let mut iter = user_id.split(':');
         let user_id = iter.next().unwrap();
         let domain = iter.next().ok_or(Error::Unknown("Domain must be provided".to_string()))?;
         (user_id, domain)
     };
-    if domain != cx.state().config.domain {
+    if domain != state.config.domain {
         return Err(Error::Unimplemented);
     }
 
-    let mut db = cx.state().db_pool.get_client().await?;
+    let mut db = state.db_pool.get_client().await?;
     let avatar_url = match db.get_profile(user_id).await?.0 {
         Some(v) => v,
         None => return Err(Error::NotFound),
     };
 
-    Ok(response::json(json!({
+    Ok(Json(json!({
         "avatar_url": avatar_url
     })))
 }
 
-pub async fn set_avatar_url(mut cx: Context<Arc<ServerState>>) -> ClientResult {
-    let mut db = cx.state().db_pool.get_client().await?;
-    let access_token = get_access_token(&cx)?;
-    let user_id = db.try_auth(access_token).await?;
-    let body: serde_json::Value = cx.body_json().await?;
+#[post("/profile/{user_id}/avatar_url")]
+pub async fn set_avatar_url(
+    state: Data<Arc<ServerState>>,
+    token: AccessToken,
+    body: Json<JsonValue>
+) -> Result<Json<()>, Error> {
+    let mut db = state.db_pool.get_client().await?;
+    let user_id = db.try_auth(token.0).await?;
     let avatar_url = body
         .get("avatar_url").ok_or(Error::BadJson(String::from("no avatar_url field")))?
         .as_str().ok_or(Error::BadJson(String::from("avatar_url should be a string")))?;
     db.set_avatar_url(&user_id, avatar_url).await?;
-    Ok(response::json(json!({})))
+    Ok(Json(()))
 }
 
-pub async fn get_display_name(cx: Context<Arc<ServerState>>) -> ClientResult {
-    let user_param = cx.param::<String>("user_id")?;
-    let user_id = percent_decode_str(&user_param).decode_utf8()
-        .map_err(|e| format!("{}", e)).map_err(Error::InvalidParam)?;
+#[post("/profile/{user_id}/displayname")]
+pub async fn get_display_name(
+    state: Data<Arc<ServerState>>,
+    user_id: Path<String>
+) -> Result<Json<JsonValue>, Error> {
     let user_id = user_id.trim_start_matches('@');
-    let (username, domain) = {
+    let (_username, domain) = {
         let mut iter = user_id.split(':');
         let username = iter.next().unwrap();
         let domain = iter.next().ok_or(Error::Unknown("Domain must be provided".to_string()))?;
         (username, domain)
     };
-    if domain != cx.state().config.domain {
+    if domain != state.config.domain {
         return Err(Error::Unimplemented);
     }
 
-    let mut db = cx.state().db_pool.get_client().await?;
+    let mut db = state.db_pool.get_client().await?;
     let display_name = match db.get_profile(user_id).await?.1 {
         Some(v) => v,
         None => return Err(Error::NotFound),
     };
 
-    Ok(response::json(json!({
+    Ok(Json(json!({
         "displayname": display_name
     })))
 }
 
-pub async fn set_display_name(mut cx: Context<Arc<ServerState>>) -> ClientResult {
-    let mut db = cx.state().db_pool.get_client().await?;
-    let access_token = get_access_token(&cx)?;
-    let user_id = db.try_auth(access_token).await?;
-    let body: serde_json::Value = cx.body_json().await?;
+#[post("/profile/{user_id}/displayname")]
+pub async fn set_display_name(
+    state: Data<Arc<ServerState>>,
+    token: AccessToken,
+    body: Json<JsonValue>
+) -> Result<Json<()>, Error> {
+    let mut db = state.db_pool.get_client().await?;
+    let user_id = db.try_auth(token.0).await?;
     let display_name = body
         .get("displayname").ok_or(Error::BadJson(String::from("no displayname field")))?
         .as_str().ok_or(Error::BadJson(String::from("displayname should be a string")))?;
     db.set_display_name(&user_id, &display_name).await?;
-    Ok(response::json(json!({})))
+    Ok(Json(()))
 }
 
-pub async fn get_profile(cx: Context<Arc<ServerState>>) -> ClientResult {
-    let user_param: String = cx.param("user_id")?;
-    let user_id = percent_decode_str(&user_param).decode_utf8()
-        .map_err(|e| format!("{}", e)).map_err(Error::InvalidParam)?;
+#[get("profile/{user_id}")]
+pub async fn get_profile(
+    state: Data<Arc<ServerState>>,
+    user_id: Path<String>
+) -> Result<Json<JsonValue>, Error> {
     let user_id = user_id.trim_start_matches('@');
-    let (username, domain) = {
+    let (_username, domain) = {
         let mut iter = user_id.split(':');
         let username = iter.next().unwrap();
         let domain = iter.next().ok_or(Error::Unknown("Domain must be provided".to_string()))?;
         (username, domain)
     };
-    if domain != cx.state().config.domain {
+    if domain != state.config.domain {
         return Err(Error::Unimplemented);
     }
 
-    let mut db = cx.state().db_pool.get_client().await?;
+    let mut db = state.db_pool.get_client().await?;
     let (avatar_url, display_name) = db.get_profile(user_id).await?;
     let mut response = serde_json::Map::new();
     if let Some(v) = avatar_url {
@@ -113,5 +123,5 @@ pub async fn get_profile(cx: Context<Arc<ServerState>>) -> ClientResult {
         response.insert("display_name".into(), v.into());
     }
 
-    Ok(response::json(response))
+    Ok(Json(response.into()))
 }
