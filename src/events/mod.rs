b55bfe7d28ed2e384f6b5c31e8cfcf025ba8feeb
Tommy Hudson <thomhuds@protonmail.com>
2020-01-15 12:25:05 +0000

    Rearrange struct fields to be more consistent

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -7,22 +7,29 @@ pub mod room;
 
 #[derive(Debug, Serialize)]
 pub struct Event {
-    pub content: JsonValue,
-    #[serde(rename = "type")]
-    pub ty: String,
-    pub event_id: String,
     /// Sometimes this is present outside this struct, in which case None is used
     #[serde(default)]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub room_id: Option<String>,
     pub sender: String,
-    pub origin_server_ts: i64,
+    #[serde(rename = "type")]
+    pub ty: String,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub state_key: Option<String>,
+    pub content: JsonValue,
     #[serde(default)]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub unsigned: Option<JsonValue>,
     #[serde(default)]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub state_key: Option<String>,
+    pub redacts: Option<String>,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub event_id: Option<String>,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub origin_server_ts: Option<i64>,
 }
 
 /// An unhashed (incomplete) Persistent Data Unit for room version 4.
@@ -31,34 +38,34 @@ pub struct Event {
 pub struct UnhashedPdu {
     pub room_id: String,
     pub sender: String,
-    pub origin: String,
-    pub origin_server_ts: i64,
     #[serde(rename = "type")]
     pub ty: String,
     pub state_key: Option<String>,
     pub content: JsonValue,
+    #[serde(skip)]
+    pub unsigned: Option<JsonValue>,
+    pub redacts: Option<String>,
+    pub origin: String,
+    pub origin_server_ts: i64,
     pub prev_events: Vec<String>,
     pub depth: i64,
     pub auth_events: Vec<String>,
-    pub redacts: Option<String>,
-    #[serde(skip)]
-    pub unsigned: Option<Map<String, JsonValue>>,
 }
 
 /// A Persistent Data Unit (room event) for room version 4.
 pub struct PduV4 {
     pub room_id: String,
     pub sender: String,
-    pub origin: String,
-    pub origin_server_ts: i64,
     pub ty: String,
     pub state_key: Option<String>,
     pub content: JsonValue,
+    pub unsigned: Option<JsonValue>,
+    pub redacts: Option<String>,
+    pub origin: String,
+    pub origin_server_ts: i64,
     pub prev_events: Vec<String>,
     pub depth: i64,
     pub auth_events: Vec<String>,
-    pub redacts: Option<String>,
-    pub unsigned: Option<Map<String, JsonValue>>,
     pub hashes: EventHash,
     pub signatures: Map<String, JsonValue>,
 }
@@ -78,16 +85,16 @@ impl UnhashedPdu {
         PduV4 {
            room_id: self.room_id,
            sender: self.sender,
-           origin: self.origin,
-           origin_server_ts: self.origin_server_ts,
            ty: self.ty,
            state_key: self.state_key,
            content: self.content,
+           unsigned: self.unsigned,
+           redacts: self.redacts,
+           origin: self.origin,
+           origin_server_ts: self.origin_server_ts,
            prev_events: self.prev_events,
            depth: self.depth,
            auth_events: self.auth_events,
-           redacts: self.redacts,
-           unsigned: self.unsigned,
            hashes: EventHash { sha256 },
            signatures: Map::new(),
         }
