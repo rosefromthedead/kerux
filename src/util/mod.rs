5264019b8e0c53bbf1ce6546f187871290c6e342
Tommy Hudson <thomhuds@protonmail.com>
2020-02-10 07:07:02 +0000

    Begin MatrixId struct

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,9 +3,11 @@ use std::sync::Arc;
 
 use crate::{storage::{Storage, StorageManager}, ServerState};
 
+pub mod mxid;
 pub mod storage;
 
 pub use storage::StorageExt;
+pub use mxid::MatrixId;
 
 #[post("/_debug/print_the_world")]
 pub async fn print_the_world(state: Data<Arc<ServerState>>) -> String {
