8065d4f1e0df03f292fab1cab597eebc5fc680bd
Rose Hudson <rose@krx.sh>
2020-02-11 15:41:30 +0000

    Implement validation of Matrix IDs

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -7,7 +7,7 @@ pub mod mxid;
 pub mod storage;
 
 pub use storage::StorageExt;
-pub use mxid::MatrixId;
+pub use mxid::{MatrixId, MxidError};
 
 #[post("/_debug/print_the_world")]
 pub async fn print_the_world(state: Data<Arc<ServerState>>) -> String {
